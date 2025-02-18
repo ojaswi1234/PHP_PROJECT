4312b6d2fc4e11ebe4b6a7149954e7fc3061a6d8
Ojaswi <ojaswideep2020@gmail.com>
2025-02-18 22:12:26 +0530

    removed buttons

--- pages/main.php
+++ pages/main.php
@@ -19,15 +19,12 @@
             <a href="#" id="button-1">About Sleep<span></span></a>
             <a href="#" id="button-2">Contact<span></span></a>
             <a href="../pages/tracker.php" id="button-3">Track My Sleep</a>
-            <a href="#" id="button-4">Resources</a>
-            <a href="#" id="button-5">Contact Us</a>
+            <a href="#" id="button-4">Contact Us</a>
+            <a href="#" id="button-5">Get Started</a>
         </nav>
         <nav id="mobile-nav">
             <button>MENU<svg aria-hidden="true" data-prefix="fal" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-bars fa-w-14 fa-7x"><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z" class=""></path></svg></button>
         </nav>
-        <div id="auth-div">
-        <a href="../pages/login.php">Login</a>
-        </div>
 
     </header>
     <main>
