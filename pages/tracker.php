c51bb420bf0c021e372d38dbf3be5c2473e86a13
Ojaswi <ojaswideep2020@gmail.com>
2025-02-18 23:50:52 +0530

    added footer

--- pages/tracker.php
+++ pages/tracker.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Tracker</h1>
+<header>
+        <div id="logo">
+        <img src="../public/modern_circular_icon_for_SleepSense_with_black__gray__and_white_colors-removebg-preview.png" width="40" height="40" id="hero-img" alt="">
+            <a> SLEEPSENSE</a>
+        </div>
+        <nav id="main-nav">
+            <a href="#" id="button-1">About Sleep<span></span></a>
+            <a href="#" id="button-2">Contact<span></span></a>
+            <a href="../pages/tracker.php" id="button-3">Track My Sleep</a>
+            <a href="#" id="button-4">Contact Us</a>
+            <a href="#" id="button-5">Get Started</a>
+        </nav>
+        <nav id="mobile-nav">
+            <button>MENU<!--<svg aria-hidden="true" data-prefix="fal" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-bars fa-w-14 fa-7x"><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z" class=""></path></svg>--></button>
+        </nav>
+
+    </header>
 </body>
 </html>
